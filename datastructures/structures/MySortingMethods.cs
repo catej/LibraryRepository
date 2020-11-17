4610f18c12edfe957ef3bad5864c88917c5d8efe
Jeff.A.Cate <Jeff.A.Cate@gmail.com>
2020-11-17 09:58:17 -0600

    Assignment 7 complete : sorting

--- datastructures/structures/MySortingMethods.cs
+++ datastructures/structures/MySortingMethods.cs
@@ -5,11 +5,10 @@ namespace datastructures.structures
 {
     public class MySortingMethods
     {
-        public int[] reversedToSort  = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-        public int[] everyOtherArreyToSort  = new int[] { 4, 10, 7, 8, 9, 6, 1, 5, 3, 2};
+        public int[] everyOtherArrayToSort  = new int[] { 4, 10, 7, 8, 9, 6, 1, 5, 3, 2};
         public string[] methods = new string[] { "Bubble sort", "Quick sort" };
-
-                public void Exe()
+        public Random rng = new Random();
+        public void Exe()
         {
             string prompt = "What would you like to do? ";
             string menu = UI.DisplayMenu(methods, prompt);
@@ -26,10 +25,12 @@ namespace datastructures.structures
             switch (methodChoice)
             {
                 case 0:
-                    BubbleSort(everyOtherArreyToSort);
+                    BubbleSort(everyOtherArrayToSort);
+                    ResetArray();
                     break;
                 case 1:
-                    QuickSort(reversedToSort);
+                    QuickSort(everyOtherArrayToSort);
+                    ResetArray();
                     break;
                 case 2:
                     Console.WriteLine("Exiting Sorting Methods...");
@@ -41,47 +42,109 @@ namespace datastructures.structures
             Console.Write("Array:");
             foreach (int item in array)
             {
-                Console.Write($" {item} |");
+                if (item > 9)
+                {
+                    Console.Write($" {item} |");
+                }
+                else
+                {
+                    Console.Write($"  {item} |");
+                }
             }
             Console.WriteLine();
 
         }
-        private int[] BubbleSort(int[] array)
+        private int[] BubbleSort(int[] newArray)
         {
-
             Console.Write("Starting ");
-            PrintArray(array);
+            PrintArray(newArray);
 
             int i, j, temp;
-            for (i = array.Length - 2; i >= 0 ; i--)
+            for (i = newArray.Length - 2; i >= 0 ; i--)
             {
                 for (j = 0; j <= i; j++)
                 {
-                    if (array[j] > array[j + 1])
+                    if (newArray[j] > newArray[j + 1])
                     {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
+                        temp = newArray[j];
+                        newArray[j] = newArray[j + 1];
+                        newArray[j + 1] = temp;
                     }
                 }
             }
 
             Console.Write("  Sorted ");
-            PrintArray(array);
-            return array;
+            PrintArray(newArray);
+            return newArray;
         }
         
-        private int[] QuickSort(int[] array)
+        private void QuickSort(int[] array)
         {
-            
             Console.Write("Starting ");
             PrintArray(array);
-            
-            throw new NotImplementedException();
-
-            Console.Write("Sorted ");
+            Console.Write("  Sorted ");
+            QuickSort(array,0, array.Length - 1);
             PrintArray(array);
-            return array;
+        }
+        private void QuickSort(int[] array, int low, int up)
+        {
+            int n = array.Length - 1;
+
+            if (low >= up)
+            {
+                return;
+            }
+            int p = Partition( array, low, up);
+            QuickSort(array, low, p - 1);
+            QuickSort(array, p + 1, up);
+
+        }
+        private int Partition(int[] array, int low, int up)
+        {
+            int temp, i, j, pivot;
+
+            pivot = array[low];
+
+            i = low + 1;
+            j = up;
+
+            while(i <= j)
+            {
+                while (array[i] < pivot && i < up)
+                {
+                    i++;
+                }
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+                if (i < j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            array[low] = array[j];
+            array[j] = pivot;
+            
+            return j;
+        }
+        private void ResetArray()
+        {
+            int[] array = new int[10]; 
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                array[i] = rng.Next(100);
+            }
+            everyOtherArrayToSort = array;
         }
     }
 }
